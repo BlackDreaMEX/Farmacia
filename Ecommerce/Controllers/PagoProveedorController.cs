2f4d33590ee284033a0441b35ccc6b2cdd5a1b26
Ariadna0801 <61101530+Ariadna0801@users.noreply.github.com>
2020-06-22 19:55:34 -0500

    Add files via upload

---
+++ Ecommerce/Controllers/PagoProveedorController.cs
@@ -0,0 +1,79 @@
+﻿using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ecommerce.Models;
+using System.IO;
+using Microsoft.AspNet.Identity;
+
+namespace Ecommerce.Controllers
+{
+    public class PagoProveedorController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: PagoProveedor
+        public ActionResult InicioPagoProveedor()
+        {
+            return View();
+        }
+
+        // GET: PagoProveedor
+        public ActionResult ConsultarFacturas()
+        {
+            return View();
+        }
+
+        // GET: PagoProveedor
+        public ActionResult ConsultarFactura()
+        {
+            return View();
+        }
+
+        // GET: PagoProveedor
+        public ActionResult GenerarOrdenPagoId4()
+        {
+            return View();
+        }
+
+        // GET: PagoProveedor
+        public ActionResult GenerarOrdenPagoId5()
+        {
+            return View();
+        }
+
+        // GET: PagoProveedor
+        public ActionResult GenerarOrdenPagoId6()
+        {
+            return View();
+        }
+
+        // GET: PagoProveedor
+        public ActionResult VerDetallesId4()
+        {
+            return View();
+        }
+
+        // GET: PagoProveedor
+        public ActionResult VerDetallesId5()
+        {
+            return View();
+        }
+
+        // GET: PagoProveedor
+        public ActionResult VerDetallesId6()
+        {
+            return View();
+        }
+
+        // GET: PagoProveedor
+        public ActionResult HistorialPagos()
+        {
+            return View();
+        }
+
+        
+    }
+}
